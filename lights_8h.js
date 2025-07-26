cdefe742261215cf20e8e897df045804e6f924d2
felipe1velez <95383973+felipe1velez@users.noreply.github.com>
2025-07-26 06:35:52 -0500

    Delete lights_8h.js

--- lights_8h.js
+++
@@ -1,6 +0,0 @@
-var lights_8h =
-[
-    [ "lights_control", "lights_8h.html#a9179a7d6ad1f280369c76a9a98947901", null ],
-    [ "media_movil2", "lights_8h.html#a372d65281b6b80ad5df38ef300b97497", null ],
-    [ "pwm_init_basic", "lights_8h.html#aa7bc2ced61a6e62280343b17cebc80dd", null ]
-];
